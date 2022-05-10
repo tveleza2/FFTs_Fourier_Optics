8575c9fcfe82087953f0448a571c5f6f3f93662b
tveleza2 <tveleza2@eafit.edu.co>
2022-05-10 08:59:45 -0500

    Update FFT.py

--- FFT.py
+++ FFT.py
@@ -24,12 +24,33 @@ def fft(x):
    return combined
 
 
-
+def FFT(x):
+    """
+    A recursive implementation of 
+    the 1D Cooley-Tukey FFT, the 
+    input should have a length of 
+    power of 2. 
+    """
+    N = len(x)
+    
+    if N == 1:
+        return x
+    else:
+        X_even = FFT(x[::2])
+        X_odd = FFT(x[1::2])
+        factor = \
+          np.exp(-2j*np.pi*np.arange(N)/ N)
+        
+        X = np.concatenate(\
+            [X_even+factor[:int(N/2)]*X_odd,
+             X_even+factor[int(N/2):]*X_odd])
+        return X
 
 
 x = np.linspace(-2*np.pi,2*np.pi,512)
 y = np.cos(x)
-F = fft(y)
+F = FFT(y)
+F = np.fft.fftshift(F)
 plt.plot(x,y)
 plt.show()
 plt.plot(x,np.real(F))
