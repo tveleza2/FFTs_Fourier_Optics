6a53726c15cef6e6655297c8f939d42ddd254bc4
tveleza2 <tveleza2@eafit.edu.co>
2022-05-13 09:37:38 -0500

    Update Angular Spectrum.py

--- Angular Spectrum.py
+++ Angular Spectrum.py
@@ -2,12 +2,13 @@ import numpy as np
 import FFT
 
 
-def Angular_Spectrum(Uin, z, x, y):
+def Angular_Spectrum(Uin, z, x, y,wavelength):
     Uspec = np.fft.fftshift(FFT.FFT2(np.fft.fftshift(Uin)))
-    fx = np.fft.fftfreq()
+    fx = np.fft.fftfreq(x)
     fy = np.fft.fftfreq(y)
-
-    Uout=0
+    phase = np.exp(2j*z*np.pi*np.sqrt((1/wavelength)**2 - np.power(fx, 2) - np.power(fy, 2)))
+    Uout = Uspec * phase
+    Uout= np.fft.ifftshift(FFT.FFT2(np.fft.ifftshift(Uout)))
     return Uout
 
 
