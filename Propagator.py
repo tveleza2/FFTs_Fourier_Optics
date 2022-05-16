49460513a72aadb961653dd3b291d43ba289e44f
tveleza2 <tveleza2@eafit.edu.co>
2022-05-16 18:21:35 -0500

    Punto1

--- Propagator.py
+++ Propagator.py
@@ -1,14 +1,17 @@
 import numpy as np
 import FFT
+import pdb
 
 
 def Angular_Spectrum(Uin, z, x, y, wavelength, dx=1, dy=1):
     '''
-    # Function to propagate a complex Field by the Angular spectrum theory
-    # Inputs:
-    # Uin - The input complex field
-    # z - The propagation distance
-    #    
+    # Function to propagate a complex field by the Angular spectrum methode
+    Inputs:
+
+    Uin - The input complex field
+
+    z - The propagation distance
+    #
     '''
 
     Uspec = FFT.FFT2(np.fft.fftshift(Uin))
@@ -23,4 +26,24 @@ def Angular_Spectrum(Uin, z, x, y, wavelength, dx=1, dy=1):
     Uout= FFT.FFT2(np.fft.ifftshift(Uout))
     
     return Uout
+
+
+def RS_1(Uin, z, dx=1, dy=1, wavelength=633e-9):
+    (Nmax,Mmax) = np.shape(Uin)
+    x = np.linspace(-dx*Nmax/2,dx*Nmax/2,Nmax)
+    y = np.linspace(-dy*Mmax/2,dy*Mmax/2,Mmax)
+    X,Y = np.meshgrid(x,y)
+    k=2*np.pi/wavelength
+    Uspec=np.fft.ifftshift(np.fft.fft2(np.fft.fftshift(Uin)))
+    r = np.sqrt(np.ones((Nmax,Mmax))*z**2+np.power(X,2)+np.power(Y,2))
+    h = (z/(1j*wavelength))*np.exp(1j*k*r)/np.power(r,2)
+    H = np.fft.ifftshift(np.fft.fft2(np.fft.fftshift(h)))
+    Uout = Uspec * H
+    Uout = np.fft.ifftshift(np.fft.ifft2(np.fft.ifftshift(Uout)))
+    # pdb.set_trace()
+    return Uout
+
+
+
+
 
